789aaecb47849b3cf074cb8cac608abed2080e69
Jhon Mike <developer@jhonmike.com.br>
2016-03-09 00:29:09 -0300

    ajustes

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -41,6 +41,16 @@ $(function() {
             $(this).parent().addClass('open');
     });
 
+    var topMenu = $("#guides"),
+        topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+        menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
     function fixDiv() {
         var $cache = $('aside > .menu-fixed');
         if ($(window).scrollTop() > 250)
@@ -53,6 +63,28 @@ $(function() {
                 'position': 'relative',
                 'top': 'auto'
             });
+
+        var fromTop = $(this).scrollTop()+topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function(){
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+        // Set/remove active class
+        if (menuItems.parent().parent().hasClass("sub")) {
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href='#" + id + "']").parent().addClass("active")
+                .end().filter("[href='#" + id + "']").parent().parent().parent().addClass("active");
+        } else {
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href='#" + id + "']").parent().addClass("active");
+        }
     }
     $(window).scroll(fixDiv);
     fixDiv();
